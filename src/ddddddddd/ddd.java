8bbe5dfff87ee6e258b402aad22f5b18f10a7c7e
Lee, Chaewon <kone0402@office.skhu.ac.kr>
2023-10-04 10:52:19 +0900

    1

---
+++ src/ddddddddd/ddd.java
@@ -0,0 +1,15 @@
+package ddddddddd;
+
+public class ddd {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		 }
+}
