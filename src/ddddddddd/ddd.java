bd126d4ee1cc25bd649faf6176990fbfbbee34c1
Lee, Chaewon <kone0402@office.skhu.ac.kr>
2023-10-04 11:04:42 +0900

    787

--- src/ddddddddd/ddd.java
+++ src/ddddddddd/ddd.java
@@ -16,6 +16,6 @@ public class ddd {
 		System.out.println("hello world");
 		System.out.println("hello world");
 		System.out.println("hello world");
-		System.out.println("hello w2");
+		System.out.println("hello world");
 		 }
 }
